4be0a5e3f233f443b6960fc01c5c5bd5ae1b6f1a
mvvlq <ardenmaalik@gmail.com>
2022-10-21 18:19:19 -0400

    Get User Data
    Added Dapper to project. API Library is created. User API is called upon login and gathers additional user data for app.

---
+++ DataManagerLibrary/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using DataManagerLibrary.Internal.DataAccess;
+using DataManagerLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManagerLibrary.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RetailManagerData");
+
+            return output;
+        }
+    }
+}
