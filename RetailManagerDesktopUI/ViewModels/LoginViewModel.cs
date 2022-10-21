012c714768c81cd159ce73b43996db9d27572d66
mvvlq <ardenmaalik@gmail.com>
2022-10-21 15:56:32 -0400

    Login Form Error Handling
    Added error message for bad requests on login form.

--- RetailManagerDesktopUI/ViewModels/LoginViewModel.cs
+++ RetailManagerDesktopUI/ViewModels/LoginViewModel.cs
@@ -40,6 +40,32 @@ namespace RetailManagerDesktopUI.ViewModels
             }
         }
 
+        public bool IsErrorVisible
+        {
+            get {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set 
+            { 
+                _errorMessage = value; 
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
         public bool CanLogIn
         {
             get
@@ -59,13 +85,13 @@ namespace RetailManagerDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
-
             }
             catch (Exception ex)
             {
-
-                Console.WriteLine(ex.Message);
-            }        }
+                ErrorMessage = ex.Message;
+            }        
+        }
     }
 }
